0664fca752c090ea561b9c141cf30f524b6ad37e
Cody <Cody@10.132.229.141>
2017-10-10 19:25:36 -0400

    completed crazy random sword

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -6,15 +6,11 @@
 
 #include "CrazyRandomSword.h"
 
+//Sword randomly ignores armor from 0 to half of current armor.
+
 double CrazyRandomSword::hit(double armor){
-    //double damage = hitPoints - (armor)
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
+    double damage = hitPoints - (rand() % int(.5 * armor));
+    return damage;
+
 }
