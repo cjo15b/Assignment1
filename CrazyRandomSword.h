06e08bab4f003904bfa8497f3eff9fb6f43dac49
Cody <Cody@10.132.229.141>
2017-10-10 18:23:00 -0400

    added description to crazy random sword

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -13,6 +13,9 @@
 #ifndef CRAZYRANDOMSWORD_H
 #define CRAZYRANDOMSWORD_H
 
+//define behavior of Crazy Random Sword. Sword has random hitpoints from 10 to 100. randomly ignores armor
+//from 0 to half of current armor.
+
 class CrazyRandomSword : public Weapon{
     public:
         CrazyRandomSword():Weapon("Crazy Random Sword", (rand() % 100 + 10)){}
