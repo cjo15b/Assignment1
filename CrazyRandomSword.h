e6d8916f48b7b0f98847f31a8ddb10f3a737420f
Cody <Cody@10.132.229.141>
2017-10-10 19:25:47 -0400

    completed crazy random sword

--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -8,6 +8,7 @@
 #include <string>
 #include "Weapon.h"
 #include <stdlib.h>
+#include <math.h>
 
 
 #ifndef CRAZYRANDOMSWORD_H
@@ -16,13 +17,16 @@
 //define behavior of Crazy Random Sword. Sword has random hitpoints from 10 to 100. randomly ignores armor
 //from 0 to half of current armor.
 
+
 class CrazyRandomSword : public Weapon{
     public:
-        CrazyRandomSword():Weapon("Crazy Random Sword", (rand() % 100 + 10)){}
+        
+        CrazyRandomSword():Weapon("Crazy Random Sword", rand() % 100 + 10){}
         virtual ~CrazyRandomSword(){};
         virtual double hit(double armor);
         
 };
+
 
 #endif /* CRAZYRANDOMSWORD_H */
 
