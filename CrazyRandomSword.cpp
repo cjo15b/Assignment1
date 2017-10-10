32e8b6ac16c7d384febd73b5f7b92a2298f7279b
Cody <Cody@10.132.229.141>
2017-10-10 11:30:36 -0400

    Implemented new SimpleAxe Weapon
    Created source files for CrazyRandomSword.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   CrazyRandomSword.cpp
+ * Author: Cody
+ *
+*/
+
+#include "CrazyRandomSword.h"
+
+double CrazyRandomSword::hit(double armor){
+    //double damage = hitPoints - (armor)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
